5817142a7774372aee8b93b3fd161cdab347a043
MikkelBaekgaardJoergensen <SE38IZ@student.aau.dk>
2024-11-14 17:08:54 +0100

    created a basic setup for nuxt and vue

---
+++ pages/about.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+    <h2>about Page</h2>
+    <p>lorem ipsum dolor sit amet</p>
+    <p>lorem ipsum dolor sit amet</p>
+    </div>
+ </template>
+ 
+ <script>
+    export default {
+        
+    }
+ </script>
+ 
+ <style lang="scss" scoped>
+ 
+ </style>
